89322a575fbf67c8edf5788a29d45b720dba6e57
WinWire <WinWire@DESKTOP-G05GMD1>
2022-10-14 15:08:23 +0530

    Add project files.

---
+++ Demo_webapi/Models/EmpDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+namespace Demo_webapi.Models
+{
+    public class EmpDbContext : DbContext
+    {
+        public EmpDbContext(DbContextOptions<EmpDbContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Employee>? Employees { get; set; }
+    }
+}
---
+++ Demo_webapi/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo_webapi.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }//nullable property
+       public string? Designation { get; set; }
+        [DataType(DataType.Date)]
+       public DateTime? JoinDate { get; set; }
+    }
+}
